e1806de87fc7881b8fce11ba5b4e4123b4360ab1
Arman <kushyanov_arman@mail.ru>
2020-06-19 13:23:15 +0400

    The End

--- js/main.js
+++ js/main.js
@@ -1,3 +1,39 @@
-$(function(){
+$(document).ready(function(){
+	const mMenuBtn = $(".m-menu-btn");
+	const Mmenu = $(".m-menu");
+	const tab = $(".tab");
 
+	mMenuBtn.on('click', function(){
+			Mmenu.toggleClass('active');
+			$('body').toggleClass('no-scroll');
+	});
+
+	tab.on('click', function() {
+		tab.removeClass("active");
+		$(this).toggleClass("active");
+		let activeTabContent = $(this).attr("data-target");
+		$('.tabs-content').removeClass("visible");
+		$(activeTabContent).toggleClass("visible");
+	});
+
+	var mySwiper = new Swiper ('.swiper-container', {
+    // Optional parameters
+    slidesPerView: 4,
+    spaceBetween: 25,
+    loop: true,
+    breakpoints: {
+    	992: {
+    		slidesPerView: 4,
+    	},
+    	768: {
+    		slidesPerView: 2,
+    	},
+			320: {
+    		slidesPerView: 1,
+    		  navigation: {
+    				nextEl: ".btn_next",
+				  },
+    	},
+    },
+  })
 });
